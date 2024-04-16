1b0f9a4f990800b6a5f6e97611d9c7a18bb8c1b3
manishphate <80386879+manishphate@users.noreply.github.com>
2024-04-16 14:59:34 +0530

    Update Dashboard.js

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -83,7 +83,20 @@ const Dashboard = () => {
     const [open, setOpen] = React.useState(false);
     const navigate = useNavigate();
 
-
+    React.useEffect(() => {
+        async function checkUserValidity() {
+            try {
+                await axios.get(`${process.env.REACT_APP_BACKEND_URI}/current-user`, {
+                    withCredentials: true
+                });
+        
+            } catch (error) {
+                navigate('/');
+            }
+        }
+        checkUserValidity();
+    }, [navigate])
+    
     const handleDrawerOpen = () => {
         setOpen(true);
     };
