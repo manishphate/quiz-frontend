fa7d6e34a9e78a5e76e6694512e440bd60eea1c5
manishphate <80386879+manishphate@users.noreply.github.com>
2024-04-19 19:11:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import ContinueUI from './Authentication/Continue';
 import Auth from './Authentication/Auth';
 import ForgotPassword from './Authentication/ForgotPassword';
 import ResetPassword from './Authentication/ResetPassword';
+import HomeDashboard from './Dashboard/HomeDashboard';
 
 
 
@@ -31,11 +32,15 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route exact path='/' element={<Auth />} />
-            <Route exact path='/signIn' element={<SignIn />} />
+            <Route exact path='/signin' element={<HomeDashboard />} />
+            <Route exact path='/sign-in' element={<SignIn />} />
             <Route path='/signUp' element={<SignUp />} />
             <Route path='/continue' element={<ContinueUI />} />
             <Route path='/forgot-password' element={<ForgotPassword />} />
             <Route path='/reset-password/:id/:token' element={<ResetPassword />} />
+            <Route exact path='/home-dashboard' element={<HomeDashboard />} />
+
+
 
 
             <Route path='/dashboard' element={<Dashboard />} />
